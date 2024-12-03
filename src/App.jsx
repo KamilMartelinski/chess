8aae204a2d1b7424dbaa6563fec67c8437a3e7f3
Kamil Martelinski <KamilMartelinski@proton.me>
2024-12-03 15:22:26 +0100

    idk

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import ChessBoard from './ChessBoard'
+
+function App() {
+
+  return (
+    <ChessBoard/>
+  )
+}
+
+export default App
---
+++ src/ChessBoard.jsx
@@ -0,0 +1,91 @@
+import './ChessBoard.css';
+
+function ChessBoard(){
+
+    return (
+        <>
+            <div id="8" className="chess_board_row">
+                <div id="A" className="chess_board_space white"></div>
+                <div id="B" className="chess_board_space black"></div>
+                <div id="C" className="chess_board_space white"></div>
+                <div id="D" className="chess_board_space black"></div>
+                <div id="E" className="chess_board_space white"></div>
+                <div id="F" className="chess_board_space black"></div>
+                <div id="G" className="chess_board_space white"></div>
+                <div id="H" className="chess_board_space black"></div>
+            </div>
+            <div id="7" className="chess_board_row">
+                <div id="A" className="chess_board_space black"><img src=""/></div>
+                <div id="B" className="chess_board_space white"></div>
+                <div id="C" className="chess_board_space black"></div>
+                <div id="D" className="chess_board_space white"></div>
+                <div id="E" className="chess_board_space black"></div>
+                <div id="F" className="chess_board_space white"></div>
+                <div id="G" className="chess_board_space black"></div>
+                <div id="H" className="chess_board_space white"></div>
+            </div>
+            <div id="6" className="chess_board_row">
+                <div id="A" className="chess_board_space white"></div>
+                <div id="B" className="chess_board_space black"></div>
+                <div id="C" className="chess_board_space white"></div>
+                <div id="D" className="chess_board_space black"></div>
+                <div id="E" className="chess_board_space white"></div>
+                <div id="F" className="chess_board_space black"></div>
+                <div id="G" className="chess_board_space white"></div>
+                <div id="H" className="chess_board_space black"></div>
+            </div>
+            <div id="5" className="chess_board_row">
+                <div id="A" className="chess_board_space black"></div>
+                <div id="B" className="chess_board_space white"></div>
+                <div id="C" className="chess_board_space black"></div>
+                <div id="D" className="chess_board_space white"></div>
+                <div id="E" className="chess_board_space black"></div>
+                <div id="F" className="chess_board_space white"></div>
+                <div id="G" className="chess_board_space black"></div>
+                <div id="H" className="chess_board_space white"></div>
+            </div>
+            <div id="4" className="chess_board_row">
+                <div id="A" className="chess_board_space white"></div>
+                <div id="B" className="chess_board_space black"></div>
+                <div id="C" className="chess_board_space white"></div>
+                <div id="D" className="chess_board_space black"></div>
+                <div id="E" className="chess_board_space white"></div>
+                <div id="F" className="chess_board_space black"></div>
+                <div id="G" className="chess_board_space white"></div>
+                <div id="H" className="chess_board_space black"></div>
+            </div>
+            <div id="3" className="chess_board_row">
+                <div id="A" className="chess_board_space black"></div>
+                <div id="B" className="chess_board_space white"></div>
+                <div id="C" className="chess_board_space black"></div>
+                <div id="D" className="chess_board_space white"></div>
+                <div id="E" className="chess_board_space black"></div>
+                <div id="F" className="chess_board_space white"></div>
+                <div id="G" className="chess_board_space black"></div>
+                <div id="H" className="chess_board_space white"></div>
+            </div>
+            <div id="2" className="chess_board_row">
+                <div id="A" className="chess_board_space white"></div>
+                <div id="B" className="chess_board_space black"></div>
+                <div id="C" className="chess_board_space white"></div>
+                <div id="D" className="chess_board_space black"></div>
+                <div id="E" className="chess_board_space white"></div>
+                <div id="F" className="chess_board_space black"></div>
+                <div id="G" className="chess_board_space white"></div>
+                <div id="H" className="chess_board_space black"></div>
+            </div>
+            <div id="1" className="chess_board_row">
+                <div id="A" className="chess_board_space black"></div>
+                <div id="B" className="chess_board_space white"></div>
+                <div id="C" className="chess_board_space black"></div>
+                <div id="D" className="chess_board_space white"></div>
+                <div id="E" className="chess_board_space black"></div>
+                <div id="F" className="chess_board_space white"></div>
+                <div id="G" className="chess_board_space black"></div>
+                <div id="H" className="chess_board_space white"></div>
+            </div>
+        </>
+    )
+}
+
+export default ChessBoard
